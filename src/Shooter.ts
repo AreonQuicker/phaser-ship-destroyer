e0a771696a401fe51de377da85a18ed2b04499e3
Gideon Harmse <GideonHarmse0404@gmail.com>
2023-02-01 12:40:06 +0200

    Updates

--- src/Shooter.ts
+++ src/Shooter.ts
@@ -1,31 +1,36 @@
-import Phaser from 'phaser';
+import Phaser from 'phaser'
 
 export class Shooter extends Phaser.GameObjects.Image {
-  public currentBody!: Phaser.Physics.Arcade.Body;
+  public currentBody!: Phaser.Physics.Arcade.Body
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
-    super(scene, x, y, texture, frame);
+    super(scene, x, y, texture, frame)
   }
 
   public addToScene() {
-    this.currentBody = this.body as Phaser.Physics.Arcade.Body;
-    this.currentBody.setSize(20, 20);
-    this.currentBody.setCollideWorldBounds(true);
-    this.currentBody.onWorldBounds = true;
+    this.currentBody = this.body as Phaser.Physics.Arcade.Body
+    this.currentBody.setSize(20, 20)
+    this.currentBody.setCollideWorldBounds(true)
+    this.currentBody.onWorldBounds = true
 
     this.scene.physics.world.on(
       'worldbounds',
-      (body, up, down, left, right) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      (body: unknown, _1: unknown, _2: unknown, _3: unknown, _4: unknown) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
         if (body.gameObject === this) {
-          const shooter = body.gameObject as Shooter;
-          shooter.destroy();
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          const shooter = body.gameObject as Shooter
+          shooter.destroy()
         }
       },
       this,
-    );
+    )
   }
 
   public destroy(fromScene?: boolean) {
-    super.destroy(fromScene);
+    super.destroy(fromScene)
   }
 }
