656b372f820510e3542e74e1c7d8bf3bc82e9076
Gideon Harmse <GideonHarmse0404@gmail.com>
2023-02-01 14:07:31 +0200

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [],
-	clearScreen: false,
-})
+  plugins: [],
+  clearScreen: false,
+  server: {
+    port: Number(process.env.PORT) || 8000,
+  },
+});
